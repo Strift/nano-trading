ba5ae4c86dfb9df9436e6af765e9dcfe4f737a6c
Matthieu <matthieu.brillaxis@live.com>
2021-10-27 15:05:24 +0200

    new diget

---
+++ widget/src/constants.tsx
@@ -0,0 +1,73 @@
+const outComeTowerOptions = [
+  {
+    id: 1,
+    value: 0,
+    odds: 5.4,
+  },
+  {
+    id: 2,
+    value: 1,
+    odds: 5.2,
+  },
+  {
+    id: 3,
+    value: 2,
+    odds: 3.7,
+  },
+  {
+    id: 4,
+    value: 3,
+    odds: 1.8,
+  },
+];
+
+const outComeWinOptions = [
+  {
+    id: 1,
+    value: "Yes",
+    odds: 1.8,
+  },
+  {
+    id: 2,
+    value: "No",
+    odds: 1.9,
+  },
+];
+
+const betValueOptions = [
+  {
+    id: 1,
+    value: 5,
+  },
+  {
+    id: 2,
+    value: 10,
+  },
+  {
+    id: 3,
+    value: 25,
+  },
+  {
+    id: 4,
+    value: 50,
+  },
+  {
+    id: 5,
+    value: 100,
+  },
+  {
+    id: 6,
+    value: 500,
+  },
+];
+
+const MAX_DURATION = 15;
+const MIN_DURATION = 0;
+
+export {
+  outComeTowerOptions,
+  outComeWinOptions,
+  betValueOptions,
+  MAX_DURATION,
+  MIN_DURATION,
+};
